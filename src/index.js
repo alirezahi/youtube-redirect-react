416487c1cdf2f986adbc1547370d4df354d40ea5
Alireza <alireza97hi@gmail.com>
2017-07-27 15:51:37 +0430

    Search bar added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = "AIzaSyBcO12SxG_zO6l_Zgjgo-3OBrgudQ5OEOg";
 
 const App = function (){
-    return <div>hello world</div>;
+    return <div>
+        <SearchBar />
+    </div>;
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
