a79ffe29e1cb437dd5fff0bf534206948b91cec9
Alireza <alireza97hi@gmail.com>
2017-07-27 16:22:15 +0430

    Search bar function added

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input />
-};
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term : '' };
+        this.OnChangeOperation = this.OnChangeOperation.bind(this);
+    }
+    render() {
+        return (
+            <div>
+                <input onChange={this.OnChangeOperation} />
+                Value : {this.state.term}
+            </div>
+        );
+    }
+
+    OnChangeOperation(event) {
+        this.setState( {
+            term : event.target.value
+        } );
+    }
+}
 
 export default SearchBar;
